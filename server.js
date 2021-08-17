25de1e90d54ffa3c5cbcde544d87677a99ed648b
Ranjan Goyal <ranjangoyal98@gmail.com>
2021-08-17 22:49:50 +0530

    Edit Backend

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((error, req, res, next) => {
 
 mongoose
     .connect(
-        `mongodb+srv://ranjan:nDIJpUQjnnjQp8wf@cluster0.mi46t.mongodb.net/reactflix_deployed?retryWrites=true&w=majority`
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mi46t.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     )
     .then(() => {
         app.listen(process.env.PORT || 5000);
